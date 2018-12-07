b14c9ea408e6d7530d6cc938807a03635be22048
PainKiller <d.malyugin@orbita.center>
2018-12-07 14:27:52 +0300

    добавил табличку с виртуализацией

--- frontend/src/comps/views/TableView.js
+++ frontend/src/comps/views/TableView.js
@@ -1,49 +1,89 @@
+/* eslint-disable react/jsx-indent */
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { showModalWindow } from 'actions';
-import './css/menu.css';
+import { List, AutoSizer, Column, Table, InfiniteLoader } from 'react-virtualized';
+import 'react-virtualized/styles.css';
 
+const ROW_COUNT = 500000;
 
+// https://stackoverflow.com/questions/44683493/infinite-scroll-with-table-react-virtualized
 
 class TableView extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.showModal1 = this.showModal1.bind(this);
-        this.showModal2 = this.showModal2.bind(this);
-        console.log("TableView");
-    }
+    this.state = {
+        loadedRowCount: 0,
+        loadedRowsMap: {},
+        loadingRowCount: 0,
+    };
 
-    showModal1() {
-        console.log("modal1");
-        this.props.showModalWindow({ message: "some message", isModal: true });
-    }
+    this._timeoutIdMap = {};
 
-    showModal2() {
-        console.log("modal2");
-        this.props.showModalWindow({ message: "some message", isModal: false });
-    }
+    this.list = Array(ROW_COUNT).fill().map((val, id) => ({
+      id,
+      name: 'John Doe',
+      image: 'http://via.placeholder.com/40',
+      text: `ООО "Ромашка - ${id}"`
+    }));
+  }
+
+
+
+    renderRow = ({ index, key, style }) => (
+
+      <div key={key} style={style} className="row">
+        <div className="image">
+          <img src={this.list[index].image} alt="" />
+        </div>
+        <div className="content">
+          <div>{this.list[index].name}</div>
+          <div>{this.list[index].text}</div>
+        </div>
+      </div>
+
+    )
 
     render() {
-
-        return (
-            <div>
-                <Link to="/render"><button className="btnBigRed btn-pos-1">button 1</button></Link>
-                <button className="btnBigRed btn-pos-2" onClick={ this.showModal1 }>modalWindow1</button>
-                <button className="btnBigRed btn-pos-3" onClick={ this.showModal2 }>modalWindow2</button>
-            </div>
-        );
+      return (
+        <div style={{ width: '100%', height: '100%' }}>
+        <div style={{ height: 'calc(100vh - 90px)', width: '80%', margin: 'auto' }}>
+          <AutoSizer>
+            {
+                ({ width, height }) => (
+                <Table
+                  width={width}
+                  height={height}
+                  headerHeight={20}
+                  rowHeight={100}
+                  rowCount={this.list.length}
+                  rowGetter={({ index }) => this.list[index]}>
+                  <Column
+                    label="Фото"
+                    dataKey="image" />
+                  <Column
+                    label="Имя"
+                    dataKey="name" />
+                  <Column
+                    label="Текст"
+                    dataKey="text" />
+                </Table>)
+            }
+          </AutoSizer>
+        </div>
+        </div>
+      );
     }
 }
 
 
 function mapDispatchToProps(dispatch) {
-    var actions = {};
-    actions.showModalWindow = showModalWindow;
-    return bindActionCreators(actions, dispatch);
+  let actions = {};
+  // actions.showModalWindow = showModalWindow;
+  return bindActionCreators(actions, dispatch);
 }
 
 export default connect(null, mapDispatchToProps)(TableView);
